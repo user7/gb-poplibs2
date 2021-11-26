c12e3155865a56cfe4831489732e2ce2999aa0fa
Maksimov, Filipp <filipp.maksimov@renpensions.com>
2021-11-26 18:48:10 +0200

    Урок 2: Начало урока

--- app/src/main/java/ru/fylmr/poplibs_nov21/CountersModel.kt
+++ app/src/main/java/ru/fylmr/poplibs_nov21/CountersModel.kt
@@ -4,18 +4,33 @@ import androidx.annotation.IntRange
 
 class CountersModel {
 
-    private val counters = mutableListOf(0, 0, 0)
+    private val counters = Counters(0, 0, 0)
 
-    fun getCurrent(@IntRange(from = 0, to = 2) index: Int): Int {
-        return counters[index]
+    fun increment1(): Int {
+        counters.counter1 += 1
+        return counters.counter1
     }
 
-    fun increment(@IntRange(from = 0, to = 2) index: Int): Int {
-        counters[index]++
-        return getCurrent(index)
+    fun increment2(): Int {
+        counters.counter2 += 1
+        return counters.counter2
     }
 
-    fun set(index: Int, value: Int) {
-        counters[index] = value
+    fun increment3(): Int {
+        counters.counter3 += 1
+        return counters.counter3
+    }
+
+    fun set(@IntRange(from = 0, to = 2) index: Int, value: Int) = when (index) {
+        0 -> counters.counter1 = value
+        1 -> counters.counter2 = value
+        2 -> counters.counter3 = value
+        else -> error("Неверный индекс")
     }
 }
+
+data class Counters(
+    var counter1: Int,
+    var counter2: Int,
+    var counter3: Int
+)
--- app/src/main/java/ru/fylmr/poplibs_nov21/MainPresenter.kt
+++ app/src/main/java/ru/fylmr/poplibs_nov21/MainPresenter.kt
@@ -1,27 +1,23 @@
 package ru.fylmr.poplibs_nov21
 
-import androidx.annotation.IdRes
-
 class MainPresenter(
     private val view: MainView
 ) {
 
     private val model = CountersModel()
 
-    fun counterClick(@IdRes id: Int) { // todo Архитектурная ошибка
-        when (id) {
-            R.id.btn_counter1 -> {
-                val nextValue = model.increment(0)
-                view.setButtonText(0, nextValue.toString())
-            }
-            R.id.btn_counter2 -> {
-                val nextValue = model.increment(1)
-                view.setButtonText(1, nextValue.toString())
-            }
-            R.id.btn_counter3 -> {
-                val nextValue = model.increment(2)
-                view.setButtonText(2, nextValue.toString())
-            }
-        }
+    fun onCounter1Clicked() {
+        val nextValue = model.increment1()
+        view.setButton1Text(nextValue.toString())
+    }
+
+    fun onCounter2Clicked() {
+        val nextValue = model.increment2()
+        view.setButton2Text(nextValue.toString())
+    }
+
+    fun onCounter3Clicked() {
+        val nextValue = model.increment3()
+        view.setButton3Text(nextValue.toString())
     }
 }
